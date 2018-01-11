a6b5d8165bba1c4fadc0f51b9f7d01dbbe9788e0
David Deprost <dadeprost@gmail.com>
2018-01-11 21:21:24 +0100

    Get rid of Default-Serve-Mux

--- main.go
+++ main.go
@@ -15,14 +15,16 @@ import (
 )
 
 func main() {
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir("./static")))
+	mux.HandleFunc("/convert", convert)
+
 	server := http.Server{
-		Addr: "127.0.0.1:8080",
+		Addr:    "127.0.0.1:8080",
+		Handler: mux,
 	}
+
 	log.Printf("Subserver is now running on http://%s ...\n", server.Addr)
-	http.HandleFunc("/convert", convert)
-
-	http.Handle("/", http.FileServer(http.Dir("./static")))
-
 	server.ListenAndServe()
 }
 
