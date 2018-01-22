e8284f1686e7fb55670e55f9703e5e3aff0d34d1
DavidDeprost <dadeprost@gmail.com>
2018-01-22 18:26:17 +0100

    Split main.go up

---
+++ handler.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"path"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func convert(w http.ResponseWriter, r *http.Request) {
+	file, header, err := r.FormFile("subtitlefile")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	seconds, err := strconv.ParseFloat(r.FormValue("seconds"), 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	plusmin, err := strconv.ParseFloat(r.FormValue("plusmin"), 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+	seconds *= plusmin
+
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	contents := string(data)
+
+	nameIn := header.Filename
+
+	fromExt := path.Ext(nameIn)
+	if !allowedExt(fromExt) {
+		log.Fatalf("'From' extension '%s' is not allowed.", fromExt)
+	}
+	toExt := r.FormValue("to")
+	if !allowedExt(toExt) {
+		log.Fatalf("'To' extension '%s' is not allowed.", toExt)
+	}
+
+	nameOut := nameOutput(nameIn, seconds, fromExt, toExt)
+
+	// We need vtt to convert, because vtt has '.' decimals
+	// (instead of ',' decimals in srt)
+	if fromExt == ".srt" {
+		contents = toVTT(contents)
+	}
+
+	contents = convertVTT(contents, seconds)
+
+	if toExt == ".srt" {
+		contents = toSRT(contents)
+	}
+
+	attach := fmt.Sprintf("attachment; filename=%s", nameOut)
+	// copy the relevant headers and filename:
+	w.Header().Set("Content-Disposition", attach)
+	w.Header().Set("Content-Type", r.Header.Get("Content-Type"))
+	w.Header().Set("Content-Length", r.Header.Get("Content-Length"))
+
+	modtime := time.Now()
+	http.ServeContent(w, r, nameOut, modtime, strings.NewReader(contents))
+
+	return
+}
